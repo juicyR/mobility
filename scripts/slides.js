59a39f239581c13e40bab3d34c2b16c20957d518
Reginald Matovu <49374492+juicyR@users.noreply.github.com>
2022-08-02 02:55:32 +0300

    Update...

--- scripts/slides.js
+++ scripts/slides.js
@@ -28,8 +28,6 @@ arrows.forEach(button => {
         if (newIndex < 0) {newIndex = slides.length - 1;} else {
             if (newIndex >= slides.length) {newIndex = 0;}
         }
-        console.log("newIndex is " + newIndex);
-        console.log("activeSlide is " + activeSlide);
 
         slides[newIndex].dataset.active = true;
         delete activeSlide.dataset.active;
