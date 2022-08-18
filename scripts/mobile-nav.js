3fa788dcebfcc96c14128a674fff86f61b4b8b57
Reginald Matovu <49374492+juicyR@users.noreply.github.com>
2022-08-18 22:08:33 +0300

    Fixed all tasks in #6

--- scripts/mobile-nav.js
+++ scripts/mobile-nav.js
@@ -1,20 +1,42 @@
 // Mobile nav js
 const btn = document.querySelector(".mobile-nav-toggle"); // Mobile nav button
 const links = document.querySelector(".links"); // Navigation links
+const firstLink = document.querySelector(".connecting-b-link");
+const secondLink = document.querySelector(".connecting-link");
+const thirdLink = document.querySelector(".connecting-contact-link");
+const x = document.getElementsByTagName("BODY")[0];
 links.classList.add("hidden");
 
 btn.addEventListener("click", () => {
-    links.classList.toggle("hidden");
+    links.classList.remove("hidden");
 
     // Adding 'open' class changes style of the elements
     btn.querySelectorAll("span").forEach((span) => span.classList.toggle("open"));
+
     links.classList.toggle("open");
 
-    setTimeout(() => {
-        links.classList.add("visible");
-    }, 80);
+    links.classList.add("visible");
 
-    if (!links.classList.contains("open")) {
+    function disableScroll() {
+        if (!links.classList.contains("open")) {
             links.classList.remove("visible");
+            x.style.overflowY = "scroll";
+        } else {
+            x.style.overflowY = "hidden";
+        }
     }
+
+    disableScroll();
+
+    function closeNav() {
+        links.classList.remove("visible");
+        links.classList.remove("open");
+        btn.querySelectorAll("span").forEach((span) => span.classList.remove("open"));
+
+        disableScroll();
+    }
+
+    firstLink.addEventListener("click", closeNav);
+    secondLink.addEventListener("click", closeNav);
+    thirdLink.addEventListener("click", closeNav);
 });
